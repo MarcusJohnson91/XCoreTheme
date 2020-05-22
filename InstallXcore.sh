381dc1cd7dcde727b7e95f12d98b4c1af81516bb
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-05-22 11:36:29 -0400

    Xcore Light and Dark

---
+++ InstallXcore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+cp Xcore-Dark.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+cp Xcore-Light.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Xcore is a theme for Xcode
+
+- It includes both light and dark variants.
+
+- Dark: [Xcore Dark](Images/Dark.png)
+
+- Light: [Xcore Light](Images/Light.png)
